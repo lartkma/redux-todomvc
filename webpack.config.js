27a28b7b36d8a8c49c5de43717e0f7973bbe3a1a
Luis Ramirez <luis.ramirez@smartts.com.pe>
2016-10-17 10:03:41 -0500

    React dependency and React Hot Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080', // Setting the URL for the hot reload
+    'webpack/hot/only-dev-server', // Reload only the dev server
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query: {
+        plugins: ["react-hot-loader/babel"]  // Include the react-hot loader
+      }
     }]
   },
   resolve: {
@@ -18,6 +25,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true // Activate hot loading
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Wire in the hot loading plugin
+  ]
 };
